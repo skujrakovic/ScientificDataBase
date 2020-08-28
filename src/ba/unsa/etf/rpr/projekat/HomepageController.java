85c9de7af0ebd4fcdfc37c6bc322c63bbb037c29
Samija Kujrakovic <skujrakovi1@etf.unsa.ba>
2020-08-28 19:49:34 +0200

    Made changes to SignUp view & added a SignUpController

--- src/ba/unsa/etf/rpr/projekat/HomepageController.java
+++ src/ba/unsa/etf/rpr/projekat/HomepageController.java
@@ -1,12 +1,37 @@
 package ba.unsa.etf.rpr.projekat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class HomepageController {
     Button btnSignUp, btnLogIn, btnContinue;
     @FXML
     public void initialize() {
 
+    }
+    public void SignUp(ActionEvent actionEvent) {
+        Parent root = null;
+        try {
+            Stage myStage=new Stage();
+            FXMLLoader loaderr = new FXMLLoader(getClass().getResource("/fxml/signup.fxml"));
+            SignUpController sctrl=new SignUpController();
+            loaderr.setController(sctrl);
+            root = loaderr.load();
+            myStage.setTitle("Sign Up");
+            myStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            myStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/ba/unsa/etf/rpr/projekat/SignUpController.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr.projekat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+    Button btnSignUp;
+    Hyperlink linkLogIn;
+    TextField fldName, fldSurname, fldUsername, fldEmail, fldPassword;
+    @FXML
+    public void initialize() {
+
+    }
+}
