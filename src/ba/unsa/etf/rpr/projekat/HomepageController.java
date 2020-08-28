52cff58598e40c41e5bd2f5358244806434be68c
Samija Kujrakovic <skujrakovi1@etf.unsa.ba>
2020-08-28 19:31:11 +0200

    Created a view for signing up

--- src/ba/unsa/etf/rpr/projekat/HomepageController.java
+++ src/ba/unsa/etf/rpr/projekat/HomepageController.java
@@ -3,7 +3,7 @@ package ba.unsa.etf.rpr.projekat;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class Homepage {
+public class HomepageController {
     Button btnSignUp, btnLogIn, btnContinue;
     @FXML
     public void initialize() {
--- src/ba/unsa/etf/rpr/projekat/Main.java
+++ src/ba/unsa/etf/rpr/projekat/Main.java
@@ -6,20 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
-
 import static javafx.application.Application.launch;
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Homepage ctrl = new Homepage();
+        HomepageController ctrl = new HomepageController();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/homepage.fxml"));
         loader.setController(ctrl);
         Parent root = loader.load();
-        primaryStage.setTitle("Homepage");
+        primaryStage.setTitle("ScienceChest");
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.show();
         //primaryStage.setResizable(false);
